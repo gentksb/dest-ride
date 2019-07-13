5eadf98c1911d0fc2d11e75312f3910c4ec0c47c
gensobunya <osa.gensobunya@gmail.com>
2019-07-13 10:50:37 +0900

    state check

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,8 @@ const stravaconfig = {
   redirect_uri : functions.config().strava.redirecturi,
   scope : functions.config().strava.scope,
   token_url: functions.config().strava.tokenurl,
-  auth_url: functions.config().strava.oauthurl
+  auth_url: functions.config().strava.oauthurl,
+  state : 'dest-ride'
   }
 const makeStravaApiHeader = (accessToken) => {
   return {'Authorization': `Bearer ${accessToken}`}
@@ -50,23 +51,28 @@ exports.listAthleteActivities = functions.https.onRequest((request, response) =>
   })
 });
 
-//認証
+//API認可
 exports.stravaAuth = functions.https.onRequest((request, response) => {
-  response.redirect(`${stravaconfig.auth_url}?client_id=${stravaconfig.client_id}&redirect_uri=${stravaconfig.redirect_uri}&response_type=code&scope=${stravaconfig.scope}`)
+  response.redirect(`${stravaconfig.auth_url}?client_id=${stravaconfig.client_id}&redirect_uri=${stravaconfig.redirect_uri}&response_type=code&scope=${stravaconfig.scope}&state=${stravaconfig.state}`)
 });
 
 exports.stravaAuthCallback = functions.https.onRequest((request, response) => {
-  axios.post(stravaconfig.token_url ,{
-      client_id: stravaconfig.client_id,
-      client_secret: stravaconfig.client_secret,
-      code: request.query['code'],
-      grant_type: 'authorization_code'
-  })
-  .then((result) => {
-    console.log(result.data);
-    return response.redirect(`/?token=${result.data.access_token}`);
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+  if(request.query['state'] !== stravaconfig.state){
+    console.error('State Unmatch Error')
+  }else{
+    axios.post(stravaconfig.token_url ,{
+        client_id: stravaconfig.client_id,
+        client_secret: stravaconfig.client_secret,
+        code: request.query['code'],
+        grant_type: 'authorization_code',
+        state : stravaconfig.state
+    })
+    .then((result) => {
+      console.log(result.data);
+        return response.redirect(`/?token=${result.data.access_token}`);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
 });
