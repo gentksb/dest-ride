68285c3238fd7a3163aae1380d94ee38d1aaa3fe
gensobunya <osa.gensobunya@gmail.com>
2019-07-14 18:39:09 +0900

    コールバック地獄潰し完了

--- functions/index.js
+++ functions/index.js
@@ -22,108 +22,100 @@ const stravaAPI = axios.create({
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 
-//NEXT todo: 超ウルトラコールバック地獄を書き直す
 
-exports.getAthlete = functions.https.onRequest((request, response) => {
-  stravaAPI.get('/athlete',{
-    headers: makeStravaApiHeader(request.query.accessToken)
-  })
-  .then((result) => {
-    console.log(result);
-    return response.status(200).send(result.data);
-  })
-  .catch((error) => {
+exports.getAthlete = functions.https.onRequest(async (request, response) => {
+  try {
+    const athelete = await stravaAPI.get('/athlete',{
+      headers: makeStravaApiHeader(request.query.accessToken)
+    })
+    console.dir(athelete.data);
+    return response.status(200).send(athelete.data);
+  }
+  catch (error) {
     console.error(error);
     return response.status(500)
-  })
-});
+  }
+})
 
-exports.getSegmentsOfActivity = functions.https.onRequest((request, response) => {
-  stravaAPI.get(`/activities/${request.query.activityId}`,{
-    headers: makeStravaApiHeader(request.query.accessToken),
-    params: {
-      include_all_efforts: true
-    }
-  })
-  .then((result) => {
-    console.log(result);
-    return response.status(200).send(result.data.segment_efforts);
-  })
-  .catch((error) => {
+exports.getSegmentsOfActivity = functions.https.onRequest(async (request, response) => {
+  try {
+    const segmentsOfActivity = await stravaAPI.get(`/activities/${request.query.activityId}`,{
+      headers: makeStravaApiHeader(request.query.accessToken),
+      params: {
+        include_all_efforts: true
+      }
+    })
+    console.dir(segmentsOfActivity.data);
+    return response.status(200).send(segmentsOfActivity.data.segment_efforts);
+  }
+  catch(error){
     console.error(error);
     return response.status(500)
-  })
-});
+  }
+})
 
-exports.listAthleteActivities = functions.https.onRequest((request, response) => {
-  stravaAPI.get('/athlete/activities',{
-    headers: makeStravaApiHeader(request.query.accessToken),
-    params: {
-      page:1,
-      per_page:10
-    }
-  })
-  .then((result) => {
-    console.log(result);
-    return response.status(200).send(result.data);
-  })
-  .catch((error) => {
+exports.listAthleteActivities = functions.https.onRequest(async (request, response) => {
+  try {
+    const AthleteActivities = await stravaAPI.get('/athlete/activities',{
+      headers: makeStravaApiHeader(request.query.accessToken),
+      params: {
+        page:1,
+        per_page:10
+      }
+    })
+    console.dir(AthleteActivities.data);
+    return response.status(200).send(AthleteActivities.data);
+  }
+  catch(error) {
     console.error(error);
     return response.status(500)
-  })
-});
+  }
+})
 
-exports.deathStride = functions.https.onRequest((request, response) => {
-  //ローカルに渡したデータ書き換え対策のため、比較用タイムを再度取得
-  stravaAPI.get(`/activities/${request.query.activityId}`,{
-    headers: makeStravaApiHeader(request.query.accessToken),
-    params: {
-      include_all_efforts: true
-    }
-  })
-    .then(async (result) => {
+exports.deathStride = functions.https.onRequest(async (request, response) => {
+    try {
+      //ローカルに渡したデータ書き換え対策のため、比較用タイムを再度取得
+      const verifiedActiviry = await stravaAPI.get(`/activities/${request.query.activityId}`,{
+        headers: makeStravaApiHeader(request.query.accessToken),
+        params: {
+          include_all_efforts: true
+        }
+      })
       let deathStrideResult = {}
       const battleSegment = Number(request.query.segmentId);
-      const segment_efforts = result.data.segment_efforts;
+      const segment_efforts = verifiedActiviry.data.segment_efforts;
       const challengerSegment = segment_efforts.filter(segment_effort => segment_effort.segment.id === battleSegment);
 
       //デストライド処理
-      const kingDataRef = firestore.collection('kingdata').where('segment_id','==', battleSegment);
-      kingDataRef.get()
-        .then( querySnapshot => {
-          querySnapshot.forEach( (kingRecord) => {
-            console.log(kingRecord.data())
-            if (challengerSegment[0].elapsed_time < kingRecord.data().elapsed_time){
-              deathStrideResult = {
-                'result' : 'Win'
-              }
-              return response.status(200).send(deathStrideResult)
-            } if(challengerSegment[0].elapsed_time > kingRecord.data().elapsed_time){
-              deathStrideResult = {
-                'result' : 'Lose'
-              }
-              return response.status(200).send(deathStrideResult)
-            } else{
-              console.warn(`king:${kingRecord.data().king_name}(${kingRecord.data().elapsed_time}) vs challenger(${challengerSegment[0].elapsed_time})`)
-              deathStrideResult = {
-                'result' : 'No Contest'
-              }
-              return response.status(200).send(deathStrideResult)
-            }
-          });
-        })
-        .catch(err=>{
-          console.error('FirestoreAccessError:',err)
-          return response.status(500)
-        })
-      return
-    })
-    .catch((error) => {
-      console.error('AxiosError:',error);
+      const kingDataRef = await firestore.collection('kingdata').where('segment_id','==', battleSegment);
+      const kingsSegment = await kingDataRef.get();
+      console.log(kingsSegment)
+      kingsSegment.forEach( (kingRecord) => {
+        console.log(kingRecord.data())
+        if (challengerSegment[0].elapsed_time < kingRecord.data().elapsed_time){
+          deathStrideResult = {
+            'result' : 'Win'
+          }
+          return response.status(200).send(deathStrideResult)
+        } if(challengerSegment[0].elapsed_time > kingRecord.data().elapsed_time){
+          deathStrideResult = {
+            'result' : 'Lose'
+          }
+          return response.status(200).send(deathStrideResult)
+        } else{
+          console.warn(`king:${kingRecord.data().king_name}(${kingRecord.data().elapsed_time}) vs challenger(${challengerSegment[0].elapsed_time})`)
+          deathStrideResult = {
+            'result' : 'No Contest'
+          }
+          return response.status(200).send(deathStrideResult)
+        }
+      });
+    }
+    catch(error) {
+      console.error(error);
       return response.status(500)
-    })
+    }
 })
 
 
@@ -132,23 +124,23 @@ exports.stravaAuth = functions.https.onRequest((request, response) => {
   response.redirect(`${stravaconfig.auth_url}?client_id=${stravaconfig.client_id}&redirect_uri=${stravaconfig.redirect_uri}&response_type=code&scope=${stravaconfig.scope}&state=${stravaconfig.state}`)
 });
 
-exports.stravaAuthCallback = functions.https.onRequest((request, response) => {
+exports.stravaAuthCallback = functions.https.onRequest(async (request, response) => {
   if(request.query['state'] !== stravaconfig.state){
     console.error('State Unmatch Error')
   }else{
-    axios.post(stravaconfig.token_url ,{
+    try {
+      const Authentication = await axios.post(stravaconfig.token_url ,{
         client_id: stravaconfig.client_id,
         client_secret: stravaconfig.client_secret,
         code: request.query['code'],
         grant_type: 'authorization_code',
         state : stravaconfig.state
-    })
-    .then((result) => {
-      console.log(result.data);
-        return response.redirect(`/?token=${result.data.access_token}`);
-    })
-    .catch((error) => {
+      })
+      console.log(Authentication.data);
+        return response.redirect(`/?token=${Authentication.data.access_token}`);
+    }
+    catch(error){
       console.log(error);
-    })
+    }
   }
 });
